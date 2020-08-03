661b1ed1ad5f3ba8daee30a2a60bac82f0f768a8
Brenda Miranda <brendamndx@gmail.com>
2020-08-03 16:15:38 -0300

    Update routes and add SessionControllers

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,6 @@ import SessionController from './controllers/SessionController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-   return res.json({ ok: true });
-});
+routes.get('/sessions', SessionController.store);
 
 export default routes;

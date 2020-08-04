3e16294bee491e40034a80060f04f7909628fbfb
Brenda Miranda <brendamndx@gmail.com>
2020-08-04 15:41:21 -0300

    Create a new route to houses

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import SessionController from './controllers/SessionController';
+import HouseController from './controllers/HouseController';
+import multer from 'multer';
+import uploadConfig from './config/Upload';
 
 const routes = new Router();
+const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
+routes.post('/houses', upload.single('thumbnail'), HouseController.store);
 
 export default routes;
